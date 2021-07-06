a599d38eec49b32e8498961fc80693f3aa42bcf9
Thiago Marinho <tgmarinho@gmail.com>
2021-07-06 09:53:34 -0400

    teste wsl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function HomePage({ posts }) {
       </Head>
       <div tw="py-32">
         <header className="mx-auto mb-24 space-y-16">
-          <Title tw="mx-auto">@tgmarinho</Title>
+          <Title tw="mx-auto">@tgmarinho tesste</Title>
           <p tw="text-center max-w-2xl mx-auto px-8 font-serif text-2xl">
             An blog about Live and Code, by
             <br />
